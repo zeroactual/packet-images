9ead44aca4e791f71fdbe554990de0e03bd05499
David Laube <dlaube@packet.net>
2017-05-15 21:44:59 -0400

    Switch to canonical based images

---
+++ distros/ubuntu/16.04/aarch64/get-aarch64-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run this script to retrieve the daily build archive and
+# then docker build from scratch
+wget http://cdimage.ubuntu.com/ubuntu-base/xenial/daily/current/xenial-base-arm64.tar.gz
---
+++ distros/ubuntu/16.04/x86_64/get-x86_64-image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run this script to retrieve the daily build archive and 
+# then docker build from scratch
+
+wget http://cdimage.ubuntu.com/ubuntu-base/xenial/daily/current/xenial-base-amd64.tar.gz
